3a26a4d3607ac0a343f47ff0c99fdc41a4c1fe6f
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-08 09:55:33 +0200

    add: hero component.

---
+++ apps/web/src/app/home/hero/hero.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: 'home-hero',
+  templateUrl: './hero.component.html',
+})
+
+export class HeroComponent {
+}
