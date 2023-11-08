3154d6e6d5bcf47fb556abbe524185063d175a26
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-08 16:48:08 +0200

    add: update contact.

---
+++ apps/web/src/app/home/contact/contact.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "home-contact",
+  templateUrl: "./contact.component.html",
+})
+export class ContactComponent {}
