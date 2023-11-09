36a335ad39783b115865ea0678a8ee2f6546d963
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-09 09:18:16 +0200

    update: change oddComponent to projectComponent.

---
+++ apps/web/src/app/home/project/project.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "home-project",
+  templateUrl: "./project.component.html",
+})
+
+export class ProjectComponent {
+}
