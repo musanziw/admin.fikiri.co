b6781dd16107cebd38f7c47496545c3a2c7b63ff
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-13 17:18:38 +0200

    update: Use logo image.

--- apps/web/app/(auth)/components/AuthCard.tsx
+++ apps/web/app/(auth)/components/AuthCard.tsx
@@ -13,11 +13,11 @@ export function AuthCard({title, children}: AuthCardProps) {
                 <div className="m-auto space-y-8 md:w-8/12 lg:w-6/12 xl:w-6/12">
                     <Link href={'/'}>
                         <div className="flex items-center justify-center">
-                            <img src="/sdg-img.png" loading="lazy" className="w-24" alt="tailus logo"/>
+                            <img src="/logo.png" loading="lazy" className="w-24" alt="Img"/>
                         </div>
                     </Link>
                     <div
-                        className="rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 backdrop-blur-2xl">
+                        className="rounded-md border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 backdrop-blur-2xl">
                         <div className="p-8 py-12 sm:p-16">
                             <h2 className="mb-8 text-2xl font-bold">{title}</h2>
                             {children}
