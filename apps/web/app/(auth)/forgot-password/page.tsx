1be9fb9afd3704227aa53af33db9e3a140594dba
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-15 09:08:52 +0200

    update: Remove unecessary padding.

--- apps/web/app/(auth)/forgot-password/page.tsx
+++ apps/web/app/(auth)/forgot-password/page.tsx
@@ -5,18 +5,16 @@ import {Button} from "@/app/(auth)/components/Button";
 
 export default function ForgotPassowrd() {
     return (
-        <div className={'bg-gray-100'}>
-            <AuthCard title={'Mot de passe oublié ?'}>
-                <form action="" className="space-y-8 flex flex-col justify-center">
-                    <Input name={'email'} label={'Email'} placeholder={'Entrez votre email'} type={'email'}/>
-                    <Button label={'Envoyez'}/>
-                    <p className="border-t border-gray-300 pt-6 text-sm text-gray-500 dark:text-gray-400">
-                        Vous avez un compte ? <Link href={'login'} className="text-gray-950">
-                        Connectez-vous
-                    </Link>
-                    </p>
-                </form>
-            </AuthCard>
-        </div>
+        <AuthCard title={'Mot de passe oublié ?'}>
+            <form action="" className="space-y-8 flex flex-col justify-center">
+                <Input name={'email'} label={'Email'} placeholder={'Entrez votre email'} type={'email'}/>
+                <Button label={'Envoyez'}/>
+                <p className="border-t border-gray-300 pt-6 text-sm text-gray-500 dark:text-gray-400">
+                    Vous avez un compte ? <Link href={'login'} className="text-gray-950">
+                    Connectez-vous
+                </Link>
+                </p>
+            </form>
+        </AuthCard>
     )
 }
--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -5,25 +5,24 @@ import {Button} from "@/app/(auth)/components/Button";
 
 export default function Login() {
     return (
-        <div className={'bg-gray-100'}>
-            <AuthCard title={'Connectez-vous'}>
-                <form action="" className="space-y-8 flex flex-col justify-center">
-                    <Input name={'email'} label={'Email'} placeholder={'Entrez votre email'} type={'email'}/>
-                    <Input name={'password'} label={'Mot de passe'} placeholder={'Entrez votre mot de passe'}
-                           type={'password'}/>
-                    <p className="text-sm text-gray-900">
-                        <Link href={'forgot-password'}>
-                            Mot de passe oublié ?
-                        </Link>
-                    </p>
-                    <Button label={'Se connecter'}/>
-                    <p className="border-t border-gray-300 pt-6 text-sm text-gray-500">
-                        Vous n&lsquo;avez pas de compte ? <Link href={'register'} className="text-gray-950">
-                        Inscrivez-vous
+        <AuthCard title={'Connectez-vous'}>
+            <form action="" className="space-y-8 flex flex-col justify-center">
+                <Input name={'email'} label={'Email'} placeholder={'Entrez votre email'} type={'email'}/>
+                <Input name={'password'} label={'Mot de passe'} placeholder={'Entrez votre mot de passe'}
+                       type={'password'}/>
+                <p className="text-sm text-gray-900">
+                    <Link href={'forgot-password'}>
+                        Mot de passe oublié ?
                     </Link>
-                    </p>
-                </form>
-            </AuthCard>
-        </div>
+                </p>
+                <Button label={'Se connecter'}/>
+                <p className="border-t border-gray-300 pt-6 text-sm text-gray-500">
+                    Vous n&lsquo;avez pas de compte ? <Link href={'register'} className="text-gray-950">
+                    Inscrivez-vous
+                </Link>
+                </p>
+            </form>
+        </AuthCard>
+
     )
 }
