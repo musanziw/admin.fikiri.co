5305cc9118e44fead355ba97b9e74a05a5d93e59
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-15 16:05:32 +0200

    update: change solution style.

--- apps/web/app/projects/[name]/page.tsx
+++ apps/web/app/projects/[name]/page.tsx
@@ -4,11 +4,11 @@ export default function Page({params}: { params: { name: string } }) {
     return (
         <div className={`relative h-screen bg-[url('/project.webp')] bg-no-repeat bg-cover bg-center`}>
             <Topbar />
-            <div className="absolute p-10 h-full flex flex-col justify-center w-full bg-gradient-to-t from-indigo-950/95 to-indigo-950/90">
+            <div className="absolute p-10 h-full flex flex-col justify-center w-full bg-indigo-950 bg-opacity-70">
                 <h2 className={'text-white text-5xl font-semibold mb-8 uppercase'}>
                     {params.name}
                 </h2>
-                <p className={'text-gray-50 text-xl'}>
+                <p className={'text-gray-50 text-xl font-thin'}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                 </p>
             </div>
