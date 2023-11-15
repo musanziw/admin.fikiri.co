be0f496b11f6bb7b784994e5d07fa9ea30c97293
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-15 09:05:18 +0200

    update: change linear.

--- apps/web/app/components/Hero.tsx
+++ apps/web/app/components/Hero.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export function Hero() {
     return (
-        <div className={'bg-gradient-to-b from-indigo-950/50 to bg-indigo-800/60'}>
+        <div className={'bg-gradient-to-b from-indigo-950/80 to-bg-indigo-950/70'}>
             <div
                 className={'relative p-8 mx-auto container h-screen flex flex-col justify-center items-start'}>
-                <h1 className={'text-6xl font-bold text-white uppercase mb-6'}>
+                <h1 className={`text-6xl font-bold text-white mb-6`}>
                     Fikiri<span className={'text-6xl font-bold text-indigo-500'}>.</span>
                 </h1>
-                <h2 className={'text-2xl text-white mb-6'}>
+                <h2 className={`text-2xl text-white mb-6`}>
                     Cartographie des solutions locales pour accélérer l&apos;atteinte des ODD en RDC.
                 </h2>
                 <Link href={'/'} className={'px-6 py-2 inline-block rounded-md bg-indigo-500 text-white font-semibold'}>
