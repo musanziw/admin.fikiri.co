cf31c2484e64ae036cddd9a4ea64c5aefb41ed75
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-15 09:04:31 +0200

    update: change title color and bg color.

--- apps/web/app/(auth)/components/AuthCard.tsx
+++ apps/web/app/(auth)/components/AuthCard.tsx
@@ -9,7 +9,7 @@ interface AuthCardProps {
 
 export function AuthCard({title, children}: AuthCardProps) {
     return (
-        <div className="relative py-16">
+        <div className="relative py-16 bg-gray-100">
             <div className="container relative m-auto px-6 md:px-12 xl:px-40">
                 <div className="m-auto space-y-8 md:w-8/12 lg:w-6/12 xl:w-6/12">
                     <Link href={'/'}>
@@ -18,9 +18,9 @@ export function AuthCard({title, children}: AuthCardProps) {
                         </div>
                     </Link>
                     <div
-                        className="rounded-md border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 backdrop-blur-2xl">
+                        className="rounded-md border border-gray-100 bg-white shadow-2xl shadow-gray-600/30 backdrop-blur-2xl">
                         <div className="p-8 py-12 sm:p-16">
-                            <h2 className="mb-8 text-2xl font-bold">{title}</h2>
+                            <h2 className="mb-8 text-2xl font-bold text-gray-900">{title}</h2>
                             {children}
                         </div>
                     </div>
