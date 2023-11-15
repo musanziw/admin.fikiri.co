18c4035b688049c19fc0bfef0044ee3aa3fd63b0
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-15 10:09:55 +0200

    update: fix width.

--- apps/web/app/components/Partners.tsx
+++ apps/web/app/components/Partners.tsx
@@ -9,7 +9,7 @@ export default function Partners() {
             <h3 className={'text-2xl font-bold mb-5'}>
                 Nos partenaires
             </h3>
-            <p className={'mb-10 w-2/3'}>
+            <p className={'mb-10 md:w-2/3'}>
                 Nous travaillons avec des partenaires locaux et internationaux pour cartographier les solutions locales
                 en République Démocratique du Congo.
             </p>
