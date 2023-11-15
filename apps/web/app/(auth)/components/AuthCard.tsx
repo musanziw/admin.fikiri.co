bd0dc13b1c90476221518a57ad8dc4cde6e23e1a
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-15 14:06:31 +0200

    update: import images.

--- apps/web/app/(auth)/components/AuthCard.tsx
+++ apps/web/app/(auth)/components/AuthCard.tsx
@@ -1,6 +1,7 @@
 import {ReactNode} from "react";
 import Link from "next/link";
 import Image from "next/image";
+import logo from '@/public/logo.png'
 
 interface AuthCardProps {
     title: string
@@ -13,9 +14,7 @@ export function AuthCard({title, children}: AuthCardProps) {
             <div className="container relative m-auto px-6 md:px-12 xl:px-40">
                 <div className="m-auto space-y-8 md:w-8/12 lg:w-6/12 xl:w-6/12">
                     <Link href={'/'}>
-                        <div className="flex items-center justify-center">
-                            <Image src="/logo.png" loading="lazy" width={120} height={200} className={"w-auto h-auto"} alt="Img"/>
-                        </div>
+                        <Image src={logo} alt="logo"/>
                     </Link>
                     <div
                         className="rounded-md border border-gray-100 bg-white shadow-2xl shadow-gray-600/30 backdrop-blur-2xl">
