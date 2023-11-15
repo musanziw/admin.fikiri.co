1f37f27dcd2741e30c935ccba2b5afd94945c1bb
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-15 10:03:48 +0200

    update: Use image instead of bg

--- apps/web/app/components/Hero.tsx
+++ apps/web/app/components/Hero.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function Hero() {
     return (
-        <div className={'bg-gradient-to-b from-indigo-950/80 to-bg-indigo-950/70'}>
+        <div className={'relative'}>
+            <Image src={'/hero.webp'} alt={'Fikiri'} layout={'fill'} objectFit={'cover'}/>
             <div
-                className={'relative p-8 mx-auto container h-screen flex flex-col justify-center items-start'}>
+                className={'relative bg-gradient-to-b from-indigo-950/60 to-indigo-950/25 p-8 h-screen flex flex-col justify-center items-start lg:p-40'}>
                 <h1 className={`text-6xl font-bold text-white mb-6`}>
                     Fikiri<span className={'text-6xl font-bold text-indigo-500'}>.</span>
                 </h1>
-                <h2 className={`text-2xl text-white mb-6`}>
+                <h2 className={`text-2xl text-white mb-6 font-medium`}>
                     Cartographie des solutions locales pour accélérer l&apos;atteinte des ODD en RDC.
                 </h2>
                 <Link href={'/'} className={'px-6 py-2 inline-block rounded-md bg-indigo-500 text-white font-semibold'}>
