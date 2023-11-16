3a13a8e688649b78f06851d5a849e5b08a62bd71
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-16 09:56:14 +0200

    update: Fixe header.

--- apps/web/app/components/Topbar.tsx
+++ apps/web/app/components/Topbar.tsx
@@ -30,23 +30,23 @@ export default function Topbar() {
 
     return (
         <div
-            className={'absolute w-screen top-0 px-10 py-8 z-10 flex items-center justify-between'}>
+            className={'fixed bg-white shadow-xl text-gray-800 w-screen top-0 px-10 py-4 z-10 flex items-center justify-between'}>
             <div>
-                <h1 className={'text-white font-bold text-2xl'}>fikiri</h1>
+                <h1 className={'text-black font-bold text-2xl'}>fikiri</h1>
             </div>
 
             <div className={`flex flex-col gap-2 lg:hidden ${isOpen && 'active'}`}
                  onClick={() => setIsOpen(!isOpen)}>
-                <div className="h-[1px] w-6 bg-white transition-transform duration-300"></div>
-                <div className="h-[1px] w-6 bg-white transition-transform duration-300"></div>
+                <div className="h-[1px] w-6 bg-black transition-transform duration-300"></div>
+                <div className="h-[1px] w-6 bg-black transition-transform duration-300"></div>
             </div>
 
             <div
-                className={`absolute top-20 z-30 rounded-xl w-3/4 bg-indigo-950/95 transition-transform duration-300 lg:hidden ${!isOpen ? 'translate-x-full right-0' : 'right-6'}`}>
+                className={`absolute top-14 z-30 w-3/4 bg-white transition-transform duration-300 right-0 lg:hidden ${!isOpen && 'translate-x-full'}`}>
                 <div className={'text-lg p-10 flex flex-col gap-4 justify-center items-start'}>
                     {
                         LINKS.map((link, index) => (
-                            <Link href={link.path} className={'hover:text-gray-100 transition-colors duration-300 capitalize'}
+                            <Link href={link.path} className={'hover:text-gray-950 font-medium transition-colors duration-300 capitalize'}
                                   key={index}>{link.name}</Link>
                         ))
                     }
