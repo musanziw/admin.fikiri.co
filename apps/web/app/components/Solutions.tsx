6fd86d776a1bcc163870589c7d6c0ad467a73a1c
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-16 09:55:39 +0200

    update: Move filters.

--- apps/web/app/components/Solutions.tsx
+++ apps/web/app/components/Solutions.tsx
@@ -6,11 +6,6 @@ export function Solutions() {
 
     return (
         <div className={'ml-4 px-4 pt-10 pb-16 text-gray-600 border-l border-dashed lg:mx-auto lg:max-w-screen-lg'}>
-           <div className="flex items-start flex-wrap  gap-x-6 mb-6">
-               <SolutionFilter title={'Thématique'} options={['Theme 1', 'Theme 2']}/>
-               <SolutionFilter title={'Objectif'} options={['Objectif 1', 'Object 2']}/>
-               <SolutionFilter title={'Ville'} options={['Ville 1', 'Ville 2']}/>
-           </div>
 
             <h3 className={'text-2xl font-bold mb-5'}>
                 Les solutions innovantes de la communauté <span className={'text-indigo-700'}>({solutions.length})</span>
@@ -19,6 +14,12 @@ export function Solutions() {
                 Les solutions innovantes de la communauté sont des projets qui ont été mis en place par des
                 organisations locales ou des individus pour résoudre les problèmes de la communauté.
             </p>
+
+            <div className="flex items-start flex-wrap  gap-x-6 mb-6">
+                <SolutionFilter title={'Objectif'} options={['Objectif 1', 'Object 2']}/>
+                <SolutionFilter title={'Thématique'} options={['Theme 1', 'Theme 2']}/>
+                <SolutionFilter title={'Ville'} options={['Ville 1', 'Ville 2']}/>
+            </div>
 
             <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                 <SolutionCard solutions={solutions}/>
