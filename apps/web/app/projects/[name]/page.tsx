564b0fa3151c9690667c6f23ff8231976e3a1bba
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-16 15:53:33 +0200

    add: Create page per project.

--- apps/web/app/projects/[name]/page.tsx
+++ apps/web/app/projects/[name]/page.tsx
@@ -1,45 +1,41 @@
 import Topbar from "@/app/components/Topbar";
-import Image from "next/image";
-import projectImg from '@/public/project.webp'
 import {Footer} from "@/app/components/Footer";
+import projectImg from "@/public/project.webp";
+import Image from "next/image";
 
 export default function Project({params}: { params: { name: string } }) {
     return (
-        <div className={'overflow-x-hidden'}>
-            <div className={`relative mb-16 h-screen bg-[url('/project.webp')] bg-no-repeat bg-cover bg-center`}>
-                <Topbar/>
-                <div
-                    className="absolute p-10 h-full flex flex-col justify-center w-full bg-gradient-to-b from-indigo-950/70 to-indigo-950">
-                    <h2 className={'text-white text-5xl font-semibold mb-8 uppercase'}>
-                        {params.name}
-                    </h2>
-                    <p className={'text-gray-50 text-xl font-thin'}>
+        <>
+            <div className={'relative mt-20 mb-16'}>
+                <Topbar background={'bg-white'}/>
+                <div className="flex flex-col mx-6 justify-center max-w-screen-md md:mx-auto border-x border-dashed">
+                    <div className="p-4">
+                        <p className="text-sm uppercase font-bold text-gray-800 mb-6">
+                            Lubumbashi, Santé et bien-être
+                        </p>
+                        <h2 className={'text-gray-600 text-5xl font-semibold mb-8 capitalize'}>
+                            {params.name}
+                        </h2>
+
+                        <p className={'font-medium mb-8'}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
+                        </p>
+
+                    </div>
+
+                    <Image src={projectImg} alt={'Img'} className={'mb-8'}/>
+
+                    <p className={'p-4 font-medium mb-8'}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                     </p>
+
                 </div>
             </div>
-
-            <div className={'p-8 grid grid-cols-1 gap-10 md:grid-cols-2 mx-auto container md:max-w-screen-lg '}>
-
-                <div className="grid grid-rows-2 grid-cols-3 gap-4">
-
-                    <div className="col-span-2">
-                        <Image src={projectImg} alt={'Img p'} className={'rounded-xl'}/>
-                    </div>
-                    <div className="col-span-2">
-                        <Image src={projectImg} alt={'Img p'} className={'rounded-xl'}/>
-                    </div>
-                    <div className="">
-                        <Image src={projectImg} alt={'Img p'} className={'rounded-xl'}/>
-                    </div>
-                </div>
-
-                <p className={'text-gray-800'}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
-                </p>
-            </div>
-            <Footer />
-        </div>
+            <Footer/>
+        </>
     )
 }
