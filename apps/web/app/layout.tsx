9bdde2f82bb45111ae8417173c2ec90129a563e3
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-16 09:54:28 +0200

    update: Change text color.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: { children: ReactNode }) {
     return (
         <html lang="en">
-        <body className={`${roboto.className} text-neutral-50`}>{children}</body>
+        <body className={`${roboto.className} text-gray-700`}>{children}</body>
         </html>
     )
 }
