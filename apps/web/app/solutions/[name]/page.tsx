eae8dd0a3e57636eb89dc011d210ad62923090b1
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-17 03:30:08 +0200

    update: Rename project into solution.

--- apps/web/app/solutions/[name]/page.tsx
+++ apps/web/app/solutions/[name]/page.tsx
@@ -3,37 +3,38 @@ import {Footer} from "@/app/components/Footer";
 import projectImg from "@/public/project.webp";
 import Image from "next/image";
 
-export default function Project({params}: { params: { name: string } }) {
+export default function Solution({params}: { params: { name: string } }) {
     return (
         <>
-            <div className={'relative py-16'}>
+            <div className={'relative'}>
                 <Topbar background={'bg-white'}/>
                 <div className="flex flex-col mx-6 justify-center max-w-screen-md md:mx-auto border-x border-dashed">
-                    <div className="p-4 py-10">
+                    <div className="p-4 pt-28">
                         <p className="text-sm uppercase font-bold text-gray-800 mb-6">
                             Lubumbashi, Santé et bien-être
                         </p>
-                        <h2 className={'text-gray-600 text-5xl font-semibold mb-8 capitalize'}>
+                        <h2 className={'text-gray-600 text-4xl font-semibold mb-6 capitalize'}>
                             {params.name}
                         </h2>
-                        <p className={'font-medium mb-6'}>
+                        <p className={'font-medium mb-4'}>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                         </p>
                     </div>
 
-                    <Image src={projectImg} alt={'Img'} className={'mb-6'}/>
+                    <Image src={projectImg} alt={'Img'} className={'mb-4'}/>
 
-                    <p className={'p-4 font-medium mb-6'}>
+                    <p className={'p-4 font-medium pb-20'}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum.
                     </p>
 
                 </div>
+                <Footer/>
+
             </div>
-            <Footer/>
         </>
     )
 }
