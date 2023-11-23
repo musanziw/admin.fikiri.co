663992d69e578a8dcbef237d1695f773b498d92d
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-23 12:40:14 +0200

    refactor: fix code quality.

--- apps/web/app/solutions/submit/page.tsx
+++ apps/web/app/solutions/submit/page.tsx
@@ -16,10 +16,10 @@ import Topbar from "@/app/components/Topbar";
 import {Footer} from "@/app/components/Footer";
 import {SolutionSubmitCard} from "@/app/(auth)/components/SolutionSubmitCard";
 import {Button} from "@/app/(auth)/components/Button";
+import loading = toast.loading;
 
 
 const SOLUTION_URI = "/solutions";
-
 
 
 export default function SubmitProject() {
@@ -41,27 +41,21 @@ export default function SubmitProject() {
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        let data: any[] = []
         try {
             axios.get("/thematics", {
                 withCredentials: true,
             }).then(response => {
-                data = response.data.data;
-            });
-
-            if (data){
+                const data: any[] = response.data.data;
                 setOptions(
                     data.map((option: any) => ({
-                        value: option.id,
-                        label: option.name,
+                        value: option?.id,
+                        label: option?.name,
                     }))
-                );
-            }
+                )
+            });
         } catch (error) {
             console.log(error);
         }
-
-
     }, []);
 
     const addErrors = (newErrors: string) => {
@@ -77,9 +71,7 @@ export default function SubmitProject() {
         ev.preventDefault();
 
         try {
-
             setIsLoading(true);
-
             const payload = {
                 name: projectTitleRef.current?.value || "",
                 video_link: projectLienYoutubeRef.current?.value || "",
@@ -90,7 +82,7 @@ export default function SubmitProject() {
                 projectImpact: projectImpactRef.current?.value || "",
                 projectExpansion: projectExpansionRef.current?.value || "",
             };
-            const response = await axios.post(SOLUTION_URI, JSON.stringify(payload), {
+            await axios.post(SOLUTION_URI, JSON.stringify(payload), {
                 headers: {"Content-Type": "application/json"},
                 withCredentials: true,
             });
@@ -106,6 +98,7 @@ export default function SubmitProject() {
             } else {
                 addErrors("Error setting up the request");
             }
+            console.log(e)
             toast.error("Échec survenue lors de la soumission de la solution");
         } finally {
             setIsLoading(false);
@@ -113,58 +106,62 @@ export default function SubmitProject() {
     };
 
 
-
     return (
         <>
             <Topbar background={"bg-white"}/>
-            <SolutionSubmitCard title={"Soumettez votre solution"}>
-                <form onSubmit={onSubmit} className="space-y-8 flex flex-col justify-center">
-                    <div className="flex flex-col lg:flex-row gap-10 lg:text-lg">
-                        <div className="basis-1/2">
-                            <div className="space-y-2">
-                                <label htmlFor="email" className="text-gray-800">
-                                    Titre du projet
-                                </label>
-                                <input
-                                    ref={projectTitleRef}
-                                    type="text"
-                                    name="title"
-                                    placeholder="Titre du projet"
-                                    className="focus:outline-none text-sm block w-full rounded-md border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500 lg:text-lg"
-                                />
+            {
+                isLoading ? <div className={'flex flex-col items-center justofy-center h-screen'}>
+                <h2 className={'text-2xl font-semibold'}>Chargement...</h2>
+                </div> : (
+                    <SolutionSubmitCard title={"Soumettez votre solution"}>
+                        <form onSubmit={onSubmit} className="space-y-8 flex flex-col justify-center">
+                            <div className="flex flex-col lg:flex-row gap-10 lg:text-lg">
+                                <div className="basis-1/2">
+                                    <div className="space-y-2">
+                                        <label htmlFor="email" className="text-gray-800">
+                                            Titre du projet
+                                        </label>
+                                        <input
+                                            ref={projectTitleRef}
+                                            type="text"
+                                            name="title"
+                                            placeholder="Titre du projet"
+                                            className="focus:outline-none text-sm block w-full rounded-md border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500 lg:text-lg"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="basis-1/2">
+                                    <div className="space-y-2">
+                                        <label htmlFor="illustration" className="text-gray-800">
+                                            {"Lien youtube de la vidéo"}
+                                        </label>
+                                        <input
+                                            ref={projectLienYoutubeRef}
+                                            placeholder="Coller le lien youtube de la vidéo decrivant le projet"
+                                            type="text"
+                                            name="illustration"
+                                            className="focus:outline-none text-sm block w-full rounded-md border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500 lg:text-lg"
+                                        />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="basis-1/2">
-                            <div className="space-y-2">
-                                <label htmlFor="illustration" className="text-gray-800">
-                                    {"Lien youtube de la vidéo"}
-                                </label>
-                                <input
-                                    ref={projectLienYoutubeRef}
-                                    placeholder="Coller le lien youtube de la vidéo decrivant le projet"
-                                    type="text"
-                                    name="illustration"
-                                    className="focus:outline-none text-sm block w-full rounded-md border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500 lg:text-lg"
-                                />
+                            <div className="flex flex-col lg:flex-row gap-10 lg:text-lg">
+                                <div className="basis-full">
+                                    <label htmlFor="">Selectionner la Thématique</label>
+                                    <Select
+                                        isMulti
+                                        id={`${optionId}`}
+                                        options={options}
+                                        onChange={handleSelectChange}
+                                        className="h-12 rounded w-full mt-2 basic-multi-select"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    </div>
 
-                    <div className="flex flex-col lg:flex-row gap-10 lg:text-lg">
-                        <div className="basis-full">
-                            <label htmlFor="">Selectionner la Thématique</label>
-                            <Select
-                                isMulti
-                                options={options}
-                                onChange={handleSelectChange}
-                                className="h-12 rounded w-full mt-2 basic-multi-select"
-                            />
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col lg:flex-row gap-5">
-                        <div className="basis-1/2">
+                            <div className="flex flex-col lg:flex-row gap-5">
+                                <div className="basis-1/2">
               <textarea
                   ref={projectDescriptionRef}
                   name=""
@@ -172,9 +169,9 @@ export default function SubmitProject() {
                   className="focus:outline-none text-sm block w-full rounded-md h-32 border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500 lg:text-lg"
                   placeholder={"Description du projet"}
               ></textarea>
-                        </div>
+                                </div>
 
-                        <div className="basis-1/2">
+                                <div className="basis-1/2">
               <textarea
                   ref={projectSolutionRef}
                   name=""
@@ -182,11 +179,11 @@ export default function SubmitProject() {
                   className=" lg:text-lg focus:outline-none text-sm block w-full rounded-md h-32 border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500"
                   placeholder={"Votre Solution"}
               ></textarea>
-                        </div>
-                    </div>
+                                </div>
+                            </div>
 
-                    <div className="flex flex-col lg:flex-row gap-5">
-                        <div className="basis-1/2 ">
+                            <div className="flex flex-col lg:flex-row gap-5">
+                                <div className="basis-1/2 ">
               <textarea
                   ref={projectEtapeRef}
                   name=""
@@ -194,9 +191,9 @@ export default function SubmitProject() {
                   className="lg:text-lg focus:outline-none text-sm block w-full rounded-md h-32 border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500"
                   placeholder={"Etapes de votre Solution"}
               ></textarea>
-                        </div>
+                                </div>
 
-                        <div className="basis-1/2">
+                                <div className="basis-1/2">
               <textarea
                   ref={projectImpactRef}
                   name=""
@@ -204,10 +201,10 @@ export default function SubmitProject() {
                   className="lg:text-lg focus:outline-none text-sm block w-full rounded-md h-32 border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500"
                   placeholder={"Impacte et fait marquant"}
               ></textarea>
-                        </div>
-                    </div>
+                                </div>
+                            </div>
 
-                    <div>
+                            <div>
             <textarea
                 ref={projectExpansionRef}
                 name=""
@@ -215,11 +212,13 @@ export default function SubmitProject() {
                 className="lg:text-lg focus:outline-none text-sm block w-full rounded-md h-32 border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500"
                 placeholder={"Projet d'expansion"}
             ></textarea>
-                    </div>
+                            </div>
 
-                    <Button label={"Soumettre"}/>
-                </form>
-            </SolutionSubmitCard>
+                            <Button label={"Soumettre"}/>
+                        </form>
+                    </SolutionSubmitCard>
+                )
+            }
             <Footer/>
             <ToastContainer/>
         </>
