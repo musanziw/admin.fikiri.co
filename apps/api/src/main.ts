cebc980f18e33d6b3a19475c994aff4eb5427ceb
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-23 15:38:33 +0200

    update: update CORS origins.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,6 +7,16 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: Boolean(process.env.SESSION_RESAVE),
+      saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
+      cookie: { maxAge: +process.env.SESSION_COOKIE_MAX_AGE },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.enableCors({
     origin: [
       'http://localhost:3000',
@@ -18,16 +28,6 @@ async function bootstrap() {
     methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
     credentials: true,
   });
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: Boolean(process.env.SESSION_RESAVE),
-      saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
-      cookie: { maxAge: +process.env.SESSION_COOKIE_MAX_AGE },
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8000);
 }
