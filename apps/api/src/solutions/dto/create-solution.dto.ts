8cf78322307d8db01e6952d3f7730e5fae04f39b
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-23 12:38:11 +0200

    format: with prettier.

--- apps/api/src/solutions/dto/create-solution.dto.ts
+++ apps/api/src/solutions/dto/create-solution.dto.ts
@@ -1,33 +1,38 @@
-import {IsArray, IsNotEmpty} from "class-validator";
+import { IsArray, IsNotEmpty } from 'class-validator';
 
 export class CreateSolutionDto {
-    @IsNotEmpty({
-        message: 'Le nom est requis'
-    })
-    name: string
+  @IsNotEmpty({
+    message: 'Le nom est requis',
+  })
+  name: string;
 
-    @IsNotEmpty({
-        message: 'Ce champ ne peut pas être vide'
-    })
-    solved_problem: string
+  @IsNotEmpty({
+    message: 'Ce champ ne peut pas être vide',
+  })
+  solved_problem: string;
 
-    @IsNotEmpty({
-        message: 'Ce champ ne peut pas être vide'
-    })
-    solution_description: string
+  @IsNotEmpty({
+    message: 'Ce champ ne peut pas être vide',
+  })
+  solution_description: string;
 
-    @IsNotEmpty({
-        message: 'Ce champ ne peut pas être vide'
-    })
-    steps: string
+  @IsNotEmpty({
+    message: 'Ce champ ne peut pas être vide',
+  })
+  steps: string;
 
-    @IsNotEmpty({
-        message: 'Ce champ ne peut pas être vide'
-    })
-    video_link: string
+  @IsNotEmpty({
+    message: 'Ce champ ne peut pas être vide',
+  })
+  video_link: string;
 
-    @IsArray({
-        message: 'Veuillez chosir une thématique'
-    })
-    thematics: number[]
+  @IsArray({
+    message: 'Veuillez chosir une thématique',
+  })
+  thematics: number[];
+
+  @IsArray({
+    message: 'Ce champ ne peut pas être vide',
+  })
+  expansion_project: string;
 }
--- apps/api/src/solutions/entities/solution.entity.ts
+++ apps/api/src/solutions/entities/solution.entity.ts
@@ -1,29 +1,38 @@
-import {Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn} from "typeorm";
-import {Thematic} from "../../thematics/entities/thematic.entity";
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Thematic } from '../../thematics/entities/thematic.entity';
 
 @Entity()
 export class Solution {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    solved_problem: string;
+  @Column({ type: 'longtext' })
+  solved_problem: string;
 
-    @Column()
-    solution_description: string;
+  @Column({ type: 'longtext' })
+  solution_description: string;
 
-    @Column()
-    steps: string;
+  @Column({ type: 'longtext' })
+  expansion_project: string;
 
-    @Column()
-    video_link: string
+  @Column({ type: 'longtext' })
+  steps: string;
 
-    @ManyToMany(() => Thematic, thematic => thematic.solutions, {
-        cascade: true,
-    })
-    @JoinTable()
-    thematics: Thematic[];
+  @Column()
+  video_link: string;
+
+  @ManyToMany(() => Thematic, (thematic) => thematic.solutions, {
+    cascade: true,
+  })
+  @JoinTable()
+  thematics: Thematic[];
 }
