c3a019fa2ef902598ba8e3a1ec4a84b51a6a4ff3
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-24 09:10:47 +0200

    update: Update CORS config.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,30 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ValidationPipe} from '@nestjs/common';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: Boolean(process.env.SESSION_RESAVE),
-      saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
-      cookie: { maxAge: +process.env.SESSION_COOKIE_MAX_AGE },
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.enableCors({
-    // origin: [process.env.CORS_ORIGIN, 'https://fikiri.co'],
-    origin: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: true,
-  });
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8000);
+    const app = await NestFactory.create(AppModule);
+    app.use(
+        session({
+            secret: process.env.SESSION_SECRET,
+            resave: Boolean(process.env.SESSION_RESAVE),
+            saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
+            cookie: {maxAge: +process.env.SESSION_COOKIE_MAX_AGE},
+        }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.enableCors({
+        // origin: [process.env.CORS_ORIGIN, 'https://fikiri.co'],
+        origin: true,
+        allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+        credentials: true,
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(8000);
 }
 
-bootstrap().then(() => {});
+bootstrap().then(() => {
+});
