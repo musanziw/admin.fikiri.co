6818aa57a945d50b6ee724b4b2540334e3a81731
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-28 23:06:40 +0200

    update: update CORS.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -17,7 +17,11 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
   app.enableCors({
-    origin: true,
+    origin: [
+      'http://localhost:3000',
+      'https://fikiri.co',
+      'https://www.fikiri.co',
+    ],
     allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
