fd9116d0686d50c4131bffb56fbbef77baaa5778
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-28 21:27:02 +0200

    update: update CORS.

--- apps/web/app/config/axios.ts
+++ apps/web/app/config/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://musanzi-wilfried.me",
-  // baseURL: "http://localhost:8000",
+  // baseURL: "https://musanzi-wilfried.me",
+  baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json"
   },
