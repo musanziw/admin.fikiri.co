d2fc3b17b19682f43bc2a073af3666cb89042613
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-28 18:31:43 +0200

    update: update CORS.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import * as process from 'process';
-import { tr } from '@faker-js/faker';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,23 +10,19 @@ async function bootstrap() {
     session({
       secret: process.env.SESSION_SECRET,
       resave: Boolean(process.env.SESSION_RESAVE),
-      saveUninitialized: Boolean(process.env.SESSION_SAVE_UNINITIALIZED),
+      saveUninitialized: false,
       cookie: { maxAge: +process.env.SESSION_COOKIE_MAX_AGE },
     }),
   );
   app.use(passport.initialize());
   app.use(passport.session());
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://fikiri.co',
-      'https://www.fikiri.co',
-    ],
-    allowedHeaders: ['Content-Type', 'Set-Cookie', 'Authorization', 'Access-Control-Allow-Origin'],
+    origin: true,
+    allowedHeaders: ['Content-Type', 'Set-Cookie', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Access-Control-Origin'],
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
     credentials: true,
-    // preflightContinue: false,
-    // optionsSuccessStatus: 204,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   await app.listen(8000);
 }
