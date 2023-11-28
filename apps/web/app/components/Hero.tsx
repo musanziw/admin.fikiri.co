4939b92c8ab098ff6a09c4808efbec177fa02a6a
MosesK <mosesziongo@gmail.com>
2023-11-28 08:53:48 +0200

    update:

--- apps/web/app/components/Hero.tsx
+++ apps/web/app/components/Hero.tsx
@@ -1,22 +1,14 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import { Typewriter } from "react-simple-typewriter";
-// import slide1 from "@/public/sliders/slide-1.jpeg";
-// import slide2 from "@/public/sliders/slide-2.jpeg";
-// import slide3 from "@/public/sliders/slide-3.jpeg";
-// import slide4 from "@/public/sliders/slide-4.jpeg";
-// import slide5 from "@/public/sliders/slide-5.jpeg";
-// import slide6 from "@/public/sliders/slide-6.jpeg";
-// import slide7 from "@/public/sliders/slide-7.jpeg";
-// import slide8 from "@/public/sliders/slide-8.jpeg";
 import slide1 from "@/public/sliders2/COMING SOON-2.png";
 import slide2 from "@/public/sliders2/FIKIRI-POSTER-1.png";
 import slide3 from "@/public/sliders2/FIKIRI-POSTER-3.png";
 import slide4 from "@/public/sliders2/FIKIRI-POSTER-B.png";
 import slide5 from "@/public/sliders2/ODD 1-8.png";
 import slide6 from "@/public/sliders2/ODD 13 15.png";
-import slide7 from "@/public/sliders2/ODD 2-8.png"
+import slide7 from "@/public/sliders2/ODD 2-8.png";
 import slide8 from "@/public/sliders2/ODD 3-8.png";
 import slide9 from "@/public/sliders2/ODD 9.png";
 
@@ -26,15 +18,54 @@ import { Carousel } from "react-responsive-carousel";
 import Topbar from "@/app/components/Topbar";
 
 const IMAGES = [
-
-]
+  {
+    src: slide1,
+    name: "coming soon",
+  },
+  {
+    src: slide2,
+    name: "slide 2",
+  },
+  {
+    src: slide3,
+    name: "slide3",
+  },
+  {
+    src: slide4,
+    name: "slide 4",
+  },
+  {
+    src: slide5,
+    name: "slide 5",
+  },
+  {
+    src: slide6,
+    name: "slide 6",
+  },
+  {
+    src: slide7,
+    name: "slide 7",
+  },
+  {
+    src: slide8,
+    name: "slide 8",
+  },
+  {
+    src: slide9,
+    name: "slide 9",
+  },
+];
 
 export function Hero() {
   return (
     <>
       <Topbar background={"bg-white"} />
       <div className={"py-32 pt-28 bg-indigo-800 text-gray-50"}>
-        <div className={"p-8 h-full grid grid-cols-1 items-center md:grid-cols-2 md:mx-auto md:max-w-screen-lg"}>
+        <div
+          className={
+            "p-8 h-full grid grid-cols-1 items-center md:grid-cols-2 md:mx-auto md:max-w-screen-lg"
+          }
+        >
           <div className={"flex flex-col justify-center items-start"}>
             <h1 className={`text-4xl font-bold text-gray-50`}>
               <Typewriter
@@ -47,66 +78,44 @@ export function Hero() {
             <h2
               className={`text-2xl text-gray-50 mb-6 font-semibold mt-6 fade-in-1`}
             >
-              Cartographie des solutions innovantes locales pour accélérer l&apos;atteinte
-              des ODD en RDC.
+              Cartographie des solutions innovantes locales pour accélérer
+              l&apos;atteinte des ODD en RDC.
             </h2>
             <p className={"mb-10 fade-in-2"}>
-              Fikiri est une plateforme web qui vise à cartographier les solutions
-              locales en République Démocratique du Congo pour accélérer
-              l&apos;atteinte des Objectifs de Développement Durable (ODD).
+              Fikiri est une plateforme web qui vise à cartographier les
+              solutions locales en République Démocratique du Congo pour
+              accélérer l&apos;atteinte des Objectifs de Développement Durable
+              (ODD).
             </p>
-            <Link href={"/solutions/submit"} className={"px-6 fade-in-3 py-2 inline-block mb-10 rounded-md bg-indigo-500 text-white font-semibold"}>
+            <Link
+              href={"/solutions/submit"}
+              className={
+                "px-6 fade-in-3 py-2 inline-block mb-10 rounded-md bg-indigo-500 text-white font-semibold"
+              }
+            >
               Postuler maintenant
             </Link>
           </div>
 
-          <div className={"self-start md:justify-self-end md:w-3/4  relative overflow-hidden"}>
-            <Carousel autoPlay={true} interval={3000} infiniteLoop={true} showThumbs={false}>
-              <Image
-                src={slide1}
-                alt={"Slide 1"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
-              <Image
-                src={slide2}
-                alt={"Slide 2"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
-              <Image
-                src={slide3}
-                alt={"Slide 3"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
-              <Image
-                src={slide4}
-                alt={"Slide 4"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
-              <Image
-                src={slide5}
-                alt={"Slide 5"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
-              <Image
-                src={slide6}
-                alt={"Slide 6"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
-              <Image
-                src={slide7}
-                alt={"Slide 7"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
-              <Image
-                src={slide8}
-                alt={"Slide 8"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
-                            <Image
-                src={slide9}
-                alt={"Slide 9"}
-                className={"w-full h-full object-cover rounded-md"}
-              />
+          <div
+            className={
+              "self-start md:justify-self-end md:w-3/4  relative overflow-hidden"
+            }
+          >
+            <Carousel
+              autoPlay={true}
+              interval={3000}
+              infiniteLoop={true}
+              showThumbs={false}
+            >
+              {IMAGES.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image.src}
+                  alt={image.name}
+                  className={"w-full h-full object-cover rounded-md"}
+                />
+              ))}
             </Carousel>
           </div>
         </div>
