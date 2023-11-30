a8cdca7c295e07f10ba4ec4e7b309250a41f49b9
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-30 20:02:22 +0200

    update: fix url for api calls

--- apps/web/app/config/axios.ts
+++ apps/web/app/config/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://musanzi-wilfried.me",
   // baseURL: "http://localhost:8000",
+
   headers: {
     "Content-Type": "application/json",
   },
