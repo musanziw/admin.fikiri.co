8c2e8a339b820686f62cae0007fcf853df330ecd
Wilfried Musanzi <musanziw.m@gmail.com>
2023-11-30 20:09:51 +0200

    update: fix url for api calls

--- apps/web/app/config/axios.ts
+++ apps/web/app/config/axios.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://musanzi-wilfried.me",
   // baseURL: "http://localhost:8000",
-
   headers: {
     "Content-Type": "application/json",
   },
--- apps/web/app/solutions/submit/page.tsx
+++ apps/web/app/solutions/submit/page.tsx
@@ -186,7 +186,6 @@ export default function SubmitProject() {
                 ref={projectLienYoutubeRef}
                 placeholder="Coller le lien de la vidéo"
                 type="text"
-                required
                 name="illustration"
                 className="focus:outline-none text-sm block w-full rounded-md border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500 lg:text-lg"
               />
