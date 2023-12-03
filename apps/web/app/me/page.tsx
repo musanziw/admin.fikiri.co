cb771caeb4ed6ca816f4836154c25190129eb7bc
Wilfried Musanzi <musanziw.m@gmail.com>
2023-12-03 23:30:27 +0200

    fix: submit error

--- apps/web/app/me/page.tsx
+++ apps/web/app/me/page.tsx
@@ -1,17 +1,15 @@
 'use client'
 
 import Topbar from "@/app/components/Topbar";
-import { Footer } from "@/app/components/Footer";
 import axios from "@/app/config/axios";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import Image from 'next/image'
 import { Input } from "../utils/Input";
-import { Button } from "../utils/Button";
-import { useFormStatus } from "react-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from "next/navigation";
+import moment from 'moment';
 
 export default function Solution() {
     const { data: account, status } = useSession()
@@ -23,6 +21,14 @@ export default function Solution() {
     const [phoneNumber, setPhoneNumber] = useState<string>('')
     const [pending, setPending] = useState(false)
     const router = useRouter()
+
+    const STATUS = [
+        'Reçue',
+        'En attente',
+        'Cartographiée',
+        'Explorée',
+        'Experimentée'
+    ]
 
     useEffect(() => {
         if (status === 'authenticated') {
@@ -86,10 +92,14 @@ export default function Solution() {
 
                     <div className="flex flex-col items-start gap-3 mb-5">
                         <h1 className={'text-4xl font-bold text-gray-950'}>Mon compte</h1>
-                        <h2 className={'text-gray-500'}>Gérez vos solutions et vos informations personnelles</h2>
+                        <h2 className={'text-gray-500'}>
+                            Gérez vos solutions et vos informations personnelles, inscrit
+                            <span className="font-semibold ml-2">
+                                {moment(user?.createdAt).locale('fr').fromNow(false)}
+                            </span>
+                        </h2>
 
                         <div className="flex flex-col gap-4">
-                            {/* <h2 className={'text-2xl text-gray-950'}>Mon compte</h2> */}
                             <div className="flex items-center gap-2 flex-wrap">
                                 <h3 className={'text-gray-500 rounded-md bg-gray-200 px-2 font-bold py-1'}>{account?.user?.email}</h3>
                                 <h3 className={'text-gray-500'}>{account?.user?.name}</h3>
@@ -114,8 +124,6 @@ export default function Solution() {
                                         <Input name={'name'} label={'Nom'} placeholder={''} type={'name'} value={name} onChange={(e) => setName(e.target.value)} />
                                         <Input name={'address'} label={'Adresse'} placeholder={''} type={'text'} value={address} onChange={(e) => setAddress(e.target.value)} />
                                         <Input name={'phoneNumber'} label={'Téléphone'} placeholder={''} type={'text'} value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-                                        {/* <Button isLoading={pending} label={pending ? "Mise à jour..." : "Enregistrez les informations"} onclick={updateProfile} /> */}
-
                                         <button type="submit" className={`px-3 py-2 rounded-md bg-indigo-600 hover:bg-indigo-500 transition-colors duration-300 disabled:cursor-not-allowed disabled:bg-indigo-500 text-white font-semibold ${pending}`} disabled={pending}>
                                             {pending ? "Mise à jour..." : "Enregistrez les informations"}
                                         </button>
@@ -144,11 +152,35 @@ export default function Solution() {
                                         {
                                             solutions && solutions.map((solution: any, i: number) => (
                                                 <>
-                                                    <div className="px-6 overflow-hidden">
-                                                        <h2 className={'text-gray-800 text-lg font-semibold'}>
-                                                            {i + 1}. {solution.name} <span className={'bg-orange-400/30 px-6 py-1 rounded-full text-dark inline-block ml-2 font-bold text-xs'}>{solution.status.name}</span>
+                                                    <div className="overflow-hidden">
+                                                        <h2 className={'text-gray-800 text-lg font-semibold mb-4'}>
+                                                            {solution.name}, soumis
+                                                            <span className={'font-medium ml-2'}>
+                                                                {moment(solution.status.createdAt).locale('fr').fromNow(false)}
+                                                            </span>
                                                         </h2>
 
+                                                        <>
+                                                            {
+                                                                STATUS.map((status: string, i: number) => {
+
+                                                                    return (
+                                                                        <>
+                                                                            <div className="flex items-center gap-3 mb-4">
+                                                                                <span className={`inline-block rounded-full p-3 ${solution.status.id >= i ? 'bg-green-300' : 'bg-gray-300'}`}>
+                                                                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
+                                                                                        <path fillRule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
+                                                                                    </svg>
+                                                                                </span>
+
+                                                                                <h5>{status}</h5>
+                                                                            </div>
+                                                                        </>
+                                                                    )
+
+                                                                })
+                                                            }
+                                                        </>
                                                     </div>
                                                 </>
                                             ))
