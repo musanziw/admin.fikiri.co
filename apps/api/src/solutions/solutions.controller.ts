13987d3c0ac1084ba37954da5df992b7704f6e9d
Wilfried Musanzi <musanziw.m@gmail.com>
2023-12-06 13:56:44 +0200

    feat: add page query param for pagination

--- apps/api/src/solutions/solutions.controller.ts
+++ apps/api/src/solutions/solutions.controller.ts
@@ -1,50 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SolutionsService } from './solutions.service';
-import { Prisma } from '@prisma/client';
+import {Controller, Get, Post, Body, Patch, Param, Delete, Query} from '@nestjs/common';
+import {SolutionsService} from './solutions.service';
+import {Prisma} from '@prisma/client';
 
 @Controller('solutions')
 export class SolutionsController {
-  constructor(private readonly solutionsService: SolutionsService) { }
+    constructor(private readonly solutionsService: SolutionsService) {
+    }
 
-  @Post()
-  create(@Body() createSolutionDto: Prisma.SolutionCreateInput & { thematic: number, call: number, challenges: number[], user: string }) {
-    return this.solutionsService.create(createSolutionDto);
-  }
+    @Post()
+    create(@Body() createSolutionDto: Prisma.SolutionCreateInput & {
+        thematic: number,
+        call: number,
+        challenges: number[],
+        user: string
+    }) {
+        return this.solutionsService.create(createSolutionDto);
+    }
 
-  @Get('approved')
-  findApproved() {
-    return this.solutionsService.findApproved();
-  }
+    @Get('approved')
+    findApproved() {
+        return this.solutionsService.findApproved();
+    }
 
-  @Get()
-  findAll() {
-    return this.solutionsService.findAll();
-  }
+    @Get()
+    findAll(@Query('page') page: string) {
+        return this.solutionsService.findAll(+page);
+    }
 
 
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.solutionsService.findOne(+id);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.solutionsService.findOne(+id);
-  }
+    @Get('user/:email')
+    findByUser(@Param('email') email: string) {
+        return this.solutionsService.findbyUser(email);
+    }
 
-  @Get('user/:email')
-  findByUser(@Param('email') email: string) {
-    return this.solutionsService.findbyUser(email);
-  }
+    @Get('challenge/:id')
+    findByCall(@Param('id') id: string) {
+        return this.solutionsService.findByCall(+id);
+    }
 
-  @Get('challenge/:id')
-  findByCall(@Param('id') id: string) {
-    return this.solutionsService.findByCall(+id);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateSolutionDto: Prisma.SolutionUpdateInput & { status: number }) {
+        return this.solutionsService.update(+id, updateSolutionDto);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSolutionDto: Prisma.SolutionUpdateInput) {
-    return this.solutionsService.update(+id, updateSolutionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.solutionsService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.solutionsService.remove(+id);
+    }
 }
