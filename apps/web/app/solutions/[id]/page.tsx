d5caa89dba40012bc50a6ba8c3a49714ba6b0d2a
Wilfried Musanzi <musanziw.m@gmail.com>
2023-12-08 10:22:15 +0200

    refactor(web): remove unavailable properties on solution

--- apps/web/app/solutions/[id]/page.tsx
+++ apps/web/app/solutions/[id]/page.tsx
@@ -1,7 +1,6 @@
 import Topbar from "@/app/components/Topbar";
 import { Footer } from "@/app/components/Footer";
 import axios from "@/app/config/axios";
-import { VideoPlayer } from "../components/VideoPlayer";
 
 export default async function Solution({ params }: { params: { id: string } }) {
     const solution = await axios.get(`solutions/${params.id}`).then(({ data: apiResponse }) => apiResponse.data)
@@ -12,9 +11,9 @@ export default async function Solution({ params }: { params: { id: string } }) {
                 <div className="flex flex-col mx-6 justify-center max-w-screen-md md:mx-auto border-x border-dashed">
                     <div className="p-4 pt-24">
                         <p className="text-sm uppercase font-bold text-gray-800 mb-6">
-                            {solution.thematics.map((thematic: any) => thematic.name).join(', ')}
+                            {solution.thematic.name}
                         </p>
-                        <h2 className={'text-gray-600 text-4xl font-semibold mb-6 uppercase'}>
+                        <h2 className={'text-gray-600 text-2xl font-semibold mb-6 uppercase'}>
                             {solution.name}
                         </h2>
                         <h2 className={'font-semibold text-xl mb-4'}>
@@ -27,38 +26,10 @@ export default async function Solution({ params }: { params: { id: string } }) {
 
                     <div className="p-4">
                         <h2 className={'font-semibold text-xl mb-4'}>
-                            Vidéo de présentation
-                        </h2>
-                    </div>
-
-                    <div className="relative h-[420px] w-full pb-9/16">
-                        <VideoPlayer link={solution.videoLink} />
-                    </div>
-
-                    <div className="p-4">
-                        <h2 className={'font-semibold text-xl mb-4'}>
                             Problème résolu
                         </h2>
                         <p className={'font-medium mb-8'}>
-                            {solution.solvedProblem}
-                        </p>
-                        <h2 className={'font-semibold text-xl mb-4'}>
-                            Project d&apos;expension
-                        </h2>
-                        <p className={'font-medium mb-8'}>
-                            {solution.expansion}
-                        </p>
-                        <h2 className={'font-semibold text-xl mb-4'}>
-                            Impactes de la solution
-                        </h2>
-                        <p className={'font-medium mb-8'}>
-                            {solution.impact}
-                        </p>
-                        <h2 className={'font-semibold text-xl mb-4'}>
-                            Etapes
-                        </h2>
-                        <p className={'font-medium mb-8'}>
-                            {solution.steps}
+                            {solution.targetedProblem}
                         </p>
                     </div>
                 </div>
