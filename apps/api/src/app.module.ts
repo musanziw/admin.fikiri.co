a1c741dcfa0475a754245a22a2dbf31f9e480ba3
Wilfried Musanzi <musanziw.m@gmail.com>
2023-12-08 10:05:24 +0200

    refactor(api): remove imported config service because it's not used

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,28 +1,30 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { RolesModule } from './roles/roles.module';
-import { SolutionsModule } from './solutions/solutions.module';
-import { ThematicsModule } from './thematics/thematics.module';
-import { PrismaModule } from './database/prisma.module';
-import { CallsModule } from './calls/calls.module';
-import { ChallengesModule } from './challenges/challenges.module';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {AuthModule} from './auth/auth.module';
+import {UsersModule} from './users/users.module';
+import {RolesModule} from './roles/roles.module';
+import {SolutionsModule} from './solutions/solutions.module';
+import {ThematicsModule} from './thematics/thematics.module';
+import {PrismaModule} from './database/prisma.module';
+import {CallsModule} from './calls/calls.module';
+import {ChallengesModule} from './challenges/challenges.module';
+import {StatusModule} from './status/status.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    AuthModule,
-    UsersModule,
-    RolesModule,
-    SolutionsModule,
-    ThematicsModule,
-    PrismaModule,
-    CallsModule,
-    ChallengesModule
-  ],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        AuthModule,
+        UsersModule,
+        RolesModule,
+        SolutionsModule,
+        ThematicsModule,
+        PrismaModule,
+        CallsModule,
+        ChallengesModule,
+        StatusModule
+    ],
 })
-export class AppModule { }
+export class AppModule {
+}
