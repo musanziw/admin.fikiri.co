ac4f10d5adac6e237feef2310a2e789a14b18043
Wilfried Musanzi <musanziw.m@gmail.com>
2023-12-08 17:18:46 +0200

    style(web): display user image

--- apps/web/app/me/page.tsx
+++ apps/web/app/me/page.tsx
@@ -86,12 +86,12 @@ export default function Solution() {
                 <div
                     className="p-8 flex flex-col mx-6 justify-center max-w-screen-md md:mx-auto border-x border-dashed">
                     {
-                        account?.user?.image ? (
+                        (account?.user?.image || user?.profile) ? (
                             <>
                                 <div
                                     className="rounded-full mb-5 mt-20 md:h-32 md:w-32 bg-gray-100 flex items-center justify-center">
-                                    <Image src={user?.profile ? `${API_URL}/uploads/${user?.profile}` : account?.user?.image} alt={account?.user?.name || 'User image'}
-                                           width={200} height={200} className={'rounded-full object-cover'}/>
+                                    <Image src={user?.profile ? `${API_URL}/uploads/${user?.profile}` : account?.user?.image} alt={user?.name || 'User image'}
+                                           width={200} height={200} className={'rounded-full object-cover'} priority={false}/>
                                 </div>
                             </>
                         ) : <>
@@ -145,7 +145,7 @@ export default function Solution() {
                                             onupdatefiles={setFiles}
                                             allowMultiple={false}
                                             server={{
-                                                url: `${API_URL}/users/${user?.id}/image`
+                                                url: `${API_URL}users/${user?.id}/image`
                                             }}
                                             name="thumb"
                                             labelIdle='Selectionnez une image'
