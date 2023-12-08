4fca4d112ac55481dd7fa0a30a943c6ac7b7e721
Wilfried Musanzi <musanziw.m@gmail.com>
2023-12-08 16:58:05 +0200

    feat(web): use API_URL

--- apps/web/app/config/axios.ts
+++ apps/web/app/config/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import {API_URL} from "@/app/config/api";
 
 export default axios.create({
-  // baseURL: "http://localhost:4000/",
-  baseURL: "https://https://fikiri-solutions.co/",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    // baseURL: "http://localhost:4000/",
+    baseURL: API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
