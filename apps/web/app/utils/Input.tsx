e85ba4c14b91d9cee87e461acc3f6c4e2abf5548
Wilfried Musanzi <musanziw.m@gmail.com>
2023-12-11 18:06:48 +0200

    feat(web): add input validation

--- apps/web/app/utils/Input.tsx
+++ apps/web/app/utils/Input.tsx
@@ -4,13 +4,14 @@ interface InputProps {
   placeholder: string;
   type: string;
   value: string,
-  onChange?: (e: any) => void;
+  state?: number;
+  onChange: (e: any) => void;
 }
 
-export function Input({ name, label, placeholder, type, value, onChange }: InputProps) {
+export function Input({ name, label, placeholder, type, value, state, onChange }: InputProps) {
   return (
     <div className="space-y-2">
-      <label htmlFor="email" className="text-gray-800">
+      <label htmlFor={name} className="text-gray-800">
         {label}
       </label>
       <input
@@ -19,7 +20,8 @@ export function Input({ name, label, placeholder, type, value, onChange }: Input
         name={name}
         value={value}
         placeholder={placeholder}
-        className="focus:outline-none text-sm block w-full placeholder:text-sm rounded-md border border-gray-200 px-4 py-3 transition duration-300 invalid:ring-3 placeholder:text-gray-600 ring-inset invalid:ring-red-400 focus:ring-2 focus:ring-indigo-500 lg:text-lg"
+        className={`focus:outline-none text-sm block w-full placeholder:text-sm rounded-md border border-gray-200 px-4 py-3 transition duration-300 placeholder:text-gray-600 ring-inset lg:text-lg 
+         ${state === 0 && 'focus:ring-2 focus:ring-indigo-600 focus:border-transparent'} ${state === 1 && 'ring-2 ring-red-500 border-transparent'} ${state === 2 && 'ring-2 ring-emerald-300 border-transparent'}`}
       />
     </div>
   );
