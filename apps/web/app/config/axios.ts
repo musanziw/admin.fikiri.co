fb29a6abf4f2a75063fc30d917db4b7f88fdaf18
Wilfried Musanzi <musanziw.m@gmail.com>
2023-12-11 18:06:05 +0200

    refactor(web): clean up code

--- apps/web/app/config/axios.ts
+++ apps/web/app/config/axios.ts
@@ -1,10 +1,9 @@
-import axios from "axios";
-import {API_URL} from "@/app/config/api";
+import axios from 'axios';
+import { API_URL } from '@/app/config/api';
 
 export default axios.create({
-    // baseURL: "http://localhost:4000/",
-    baseURL: API_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
